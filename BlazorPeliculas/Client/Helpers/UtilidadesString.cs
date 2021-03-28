3b1fc8d8b43dfee59c885047bc0dd085fdd28864
makadown <maseflores79@gmail.com>
2021-03-28 10:32:41 -0600

    first commit

---
+++ BlazorPeliculas/Client/Helpers/UtilidadesString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public class UtilidadesString
+    {
+        public static string EnMayuscular(string valor) => valor.ToUpper();
+    }
+}
