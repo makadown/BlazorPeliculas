b2b810a6cc1fda5ada9d217bd046b456738ae533
makadown <maseflores79@gmail.com>
2021-04-03 09:08:55 -0600

    Avance formularios

--- BlazorPeliculas/Shared/Entidades/Pelicula.cs
+++ BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorPeliculas.Shared.Entidades
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
-        public DateTime Lanzamiento { get; set; }
-        /// <summary>
-        /// Url de poster de la pelicula
-        /// </summary>
+        public string Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? Lanzamiento { get; set; }
         public string Poster { get; set; }
+        public List<GeneroPelicula> GenerosPelicula { get; set; } = new List<GeneroPelicula>();
         public string TituloCortado
         {
             get
